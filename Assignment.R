89662b7f152b40dce7308f851f853ed2b57f5032
Aarth78 <parth.ajmera.phe16@iitbhu.ac.in>
2018-06-09 14:57:51 +0530

    Assignment 3
    Coursera R Programming

---
+++ Assignment.R
@@ -0,0 +1,51 @@
+best <- function(state, outcome)
+{
+  setwd("/Users/parthajmera/Desktop/Data Science/RCourse2/rprog-data-ProgAssignment3-data/")
+  state <- "TX"
+  outcome <- "heart attack"
+  
+  ##Read Data
+  
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  ##Organise Data
+  
+  betterData   <-  as.data.frame(cbind(data[, 2],   # hospital
+                                  data[, 7],   # state
+                                  data[, 11],  # heart attack
+                                  data[, 17],  # heart failure
+                                  data[, 23]), # pneumonia
+                                  stringsAsFactors = FALSE)
+  
+  colnames(betterData) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+  
+  
+  ##Verify Data
+  
+  if(!((outcome == "heart attack")|| (outcome ==  "heart failure") || (outcome == "pneumonia"))){stop("invalid outcome")}
+  if(!(state %in% unique(betterData$state))){stop("invalid state")}
+  
+  
+  
+    which_Rows_Have_State_Needed <- which(betterData[ , "state"] == state)
+    
+  
+    betterStateData <- betterData[which_Row_Have_State_Needed, ]    # extracting data for the called state
+    
+    
+    #Since data has text "Not Available", it will coerce Numerical values to text as well. So we convert it into numerics
+    
+    
+    removingTextAndIntroducing_NA <- as.numeric(betterStateData[, outcome])                 #
+    
+    min_val <- min(removingTextAndIntroducing_NA, na.rm = TRUE)
+    
+    result  <- betterStateData[, "hospital"][which(removingTextAndIntroducing_NA == min_val)]
+    
+    output  <- result[order(result)]
+    
+    result
+    
+    output
+  
+}
